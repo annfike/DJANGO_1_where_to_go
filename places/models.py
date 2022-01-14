6aeb726500f9d561943e22bfdb956c2e04e44dc9
Anna <44573387+annfike@users.noreply.github.com>
2022-01-14 12:40:32 +0300

    Add Excursion model

--- places/models.py
+++ places/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
-# Create your models here.
-print("hello!")
+
+class Excursion(models.Model):
+    title = models.CharField('Название экскурсии', max_length=50)
+    description_short = models.CharField('Краткое описание', max_length=256, blank=True, default='')
+    description_long = models.CharField('Подробное описание', max_length=256, blank=True, default='')
+    lat = models.FloatField(default=55, verbose_name='долгота')
+    lon = models.FloatField(default=55, verbose_name='широта')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Экскурсия'
+        verbose_name_plural = 'Экскурсии'
