7a96e0494e6d24b2e7ab062f1075d3bc89e3468f
Anna <44573387+annfike@users.noreply.github.com>
2022-01-16 11:17:35 +0300

    Move data from template to view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,11 +1,38 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
-def show_map(request):
-    #print('Кто-то зашёл на главную!')
-    #return HttpResponse('Здесь будет карта')
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
-    #или return render(request, 'index.html')
+def index(request):
+    excursions  = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "./static/places/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "./static/places/roofs24.json"
+          }
+        }
+      ]
+    }
+
+    context = {
+        'excursions': excursions
+    }
+
+    return render(request, 'index.html', context)
