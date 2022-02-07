b98f3846adc53712aee6233a31b9bc574760bbd5
Anna <44573387+annfike@users.noreply.github.com>
2022-02-07 18:55:06 +0300

    add /places/id/

--- places/views.py
+++ places/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from places.models import Excursion
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+def place_details(request, id):
+    place = get_object_or_404(Excursion, pk=id)
+    return HttpResponse(place.title)
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from where_to_go import views, settings
 from django.conf.urls.static import static
 from django.conf import settings
-
+#from places import views as places_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    #path('places/<int:id>/', places_views.place_details),
+    path('places/', include('places.urls', namespace='places')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
