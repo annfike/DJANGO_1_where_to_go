75edd13e04c1e461a8dbf471a2dedabe01329476
Anna <44573387+annfike@users.noreply.github.com>
2022-02-08 13:26:21 +0300

    get JsonResponse

--- places/views.py
+++ places/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from places.models import Excursion
+from places.models import Excursion, Image
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 
 def place_details(request, id):
     place = get_object_or_404(Excursion, pk=id)
-    return HttpResponse(place.title)
+    images = [image.photo.url for image in Image.objects.filter(excursion=place)]
+    response_data = {
+        "title": place.title,
+        "imgs": images,
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {"lat": place.lat, "lng": place.lon},
+    }
+    return JsonResponse(response_data, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 2})
 
