08c2fc52112f68d50f8a671741de360ab2077368
Anna <44573387+annfike@users.noreply.github.com>
2022-02-08 16:30:22 +0300

    add preview for Image

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
 from .models import Excursion, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ["preview"]
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.photo.url}" style="max-height: 100px;">')
 
 
 @admin.register(Excursion)
@@ -12,4 +15,9 @@ class ExcursionAdmin(admin.ModelAdmin):
     inlines = (ImageInline,)
 
 
-admin.site.register(Image)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    readonly_fields = ["preview"]
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.photo.url}" style="max-height: 200px;">')
+        
