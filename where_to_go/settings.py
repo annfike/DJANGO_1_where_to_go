1ebe1fe41f21136c06fe7dfe3f615235d7391c18
Anna <44573387+annfike@users.noreply.github.com>
2022-02-09 14:28:19 +0300

    set  .env, settings for deploy

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from environs import Env
+
+env = Env()
+env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%oo)hvyf%hqsmvw8cv4v2$qvlp$t-2i00z*bv@%1^&nc5#k=cz'
+SECRET_KEY = env.str('SECRET_KEY', 'empty')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', False)
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
