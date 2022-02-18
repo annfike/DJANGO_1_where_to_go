463516e0e1affb0bf76a83c36c43a84a1e0459ed
Anna <44573387+annfike@users.noreply.github.com>
2022-02-18 19:47:11 +0300

    fix Admin (format_html, raw_id_fields)

--- places/admin.py
+++ places/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.utils.safestring import mark_safe
 from .models import Excursion, Image
 from adminsortable2.admin import SortableInlineAdminMixin
+from django.utils.html import format_html
 
 
 class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     readonly_fields = ["preview"]
     def preview(self, obj):
-        return mark_safe(f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+        return format_html('<img src= {} height=100 />', mark_safe(obj.photo.url))
 
 
 @admin.register(Excursion)
@@ -21,6 +22,7 @@ class ExcursionAdmin(admin.ModelAdmin):
 class ImageAdmin(admin.ModelAdmin):
     fields = ('excursion', 'photo', 'preview', 'number')
     readonly_fields = ["preview"]
+    raw_id_fields = ("excursion",)
     def preview(self, obj):
-        return mark_safe(f'<img src="{obj.photo.url}" style="max-height: 200px;">')
+        return format_html('<img src= {} height=200 />', mark_safe(obj.photo.url))
         
