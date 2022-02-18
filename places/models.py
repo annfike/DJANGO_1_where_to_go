1ef0f534a672ed6b8c5cb6ded68f79d4f6df4dc5
Anna <44573387+annfike@users.noreply.github.com>
2022-02-18 19:24:36 +0300

    fix Models

--- places/models.py
+++ places/models.py
@@ -4,10 +4,10 @@ from tinymce.models import HTMLField
 
 class Excursion(models.Model):
     title = models.CharField('Название экскурсии', max_length=50)
-    description_short = models.CharField('Краткое описание', max_length=256, blank=True, default='')
-    description_long = HTMLField('Подробное описание', blank=True, default='')
-    lat = models.FloatField(default=55, verbose_name='долгота')
-    lon = models.FloatField(default=55, verbose_name='широта')
+    description_short = models.TextField('Краткое описание', blank=True)
+    description_long = HTMLField('Подробное описание', blank=True)
+    lat = models.FloatField(verbose_name='долгота')
+    lon = models.FloatField(verbose_name='широта')
 
     def __str__(self):
         return f'{self.title}'
@@ -20,8 +20,8 @@ class Excursion(models.Model):
 
 class Image(models.Model):
     excursion = models.ForeignKey(Excursion, on_delete=models.CASCADE, verbose_name='название', related_name='photos',)
-    photo = models.ImageField(blank=True, upload_to='images', verbose_name='картинка')
-    number = models.PositiveIntegerField('Номер', default=0, blank=False, null=False)
+    photo = models.ImageField(upload_to='images', verbose_name='картинка')
+    number = models.PositiveIntegerField('Номер', default=0, blank=True)
 
     def __str__(self):
         return f'{self.number} {self.excursion.title}'
