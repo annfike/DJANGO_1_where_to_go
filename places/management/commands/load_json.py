41bb441b9afa214801c8d6c0ce300d12b9a24cfe
Anna <44573387+annfike@users.noreply.github.com>
2022-02-28 12:24:26 +0300

    optimized load_json

--- places/management/commands/load_json.py
+++ places/management/commands/load_json.py
@@ -1,4 +1,4 @@
-import logging
+from pathlib import Path
 from django.core.management import BaseCommand
 from places.models import Excursion
 import json
@@ -9,30 +9,23 @@ import os
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('-f', '--dir', default='upload', type=str, help='Название папки с файлами JSON')
+        parser.add_argument('-f', '--dir', default='./upload', type=str, help='Путь к папке с файлами JSON')
 
     def handle(self, *args, **kwargs):
-        dir = kwargs['dir']
-        try:
-            load_json(dir)
-        except Exception as error:
-            logging.error(error)
-
-
-def load_json(dir):
-    for file in os.listdir(f'./{dir}'):
-        file = f'{dir}/{file}'
-        load_json_file(file)
-
+        dir_path = kwargs['dir']
+        for file in os.listdir(dir_path):
+            file = f'{dir_path}/{file}'
+            load_json_file(file)
+        
 
 def load_json_file(file):
     with open(file, 'r', encoding='utf-8') as f:
-        places = json.load(f)
+        excursions_raw = json.load(f)
 
-        title = places.get('title', 'There is no title')
-        description_short = places.get('description_short', 'There is no description_short')
-        description_long = places.get('description_long', 'There is no description_long')
-        coordinates = places.get('coordinates')
+        title = excursions_raw.get('title')
+        description_short = excursions_raw.get('description_short', 'description is coming soon')
+        description_long = excursions_raw.get('description_long', 'description is coming soon')
+        coordinates = excursions_raw.get('coordinates')
         if coordinates:
             lng = coordinates['lng']
             lat = coordinates['lat']
@@ -40,23 +33,24 @@ def load_json_file(file):
             lng = 37.374
             lat = 55.455
 
-        excursion, created = Excursion.objects.update_or_create(
-            title=title,
-            defaults={
-                  'description_short': description_short,
-                  'description_long': description_long,
-                  'lon': lat,
-                  'lat': lng
-            },
-            )
+        if title:
+            excursion, created = Excursion.objects.update_or_create(
+                  title=title,
+                  defaults={
+                        'description_short': description_short,
+                        'description_long': description_long,
+                        'lon': lat,
+                        'lat': lng
+                  },
+                  )
 
-        imgs = places.get('imgs')
-        for number, img in enumerate(imgs, 1):
-            response = requests.get(img)
+        imgs_urls = excursions_raw.get('imgs')
+        for number, img_url in enumerate(imgs_urls, 1):
+            response = requests.get(img_url)
             response.raise_for_status()
-            i = BytesIO(response.content)
-            image, created = excursion.photos.get_or_create(
+            img = BytesIO(response.content)
+            image, created = excursion.images.get_or_create(
                   excursion=excursion.id, number=number
             )
-            img_name = os.path.basename(img)
-            image.photo.save(img_name, i, save=True)
+            img_name = os.path.basename(img_url)
+            image.photo.save(img_name, img, save=True)
